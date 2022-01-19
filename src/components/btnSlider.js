7d112023d471d842f2de70c6989ec8a05064a2ee
ArtinCF93 <Toojool@gmail.com>
2022-01-19 12:14:24 -0800

    Update btnSlider.js

--- src/components/btnSlider.js
+++ src/components/btnSlider.js
@@ -3,7 +3,7 @@ import './btnSlider.css'
 import { SliderGreenboxData } from './SliderGreenboxData'
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
-let BtnSlider = ({slides}) => {
+const BtnSlider = ({slides}) => {
 
     //current state is defaulted at 0
     let [current, setCurrent] = useState(0);
@@ -13,7 +13,7 @@ let BtnSlider = ({slides}) => {
 // Since the greenbox array has 12 elements, it has a length of 12.
 // length - 1 means at the last element of the array, because the last index is 11 (as first index starts at 0). So 12 -1 = 11 which is the last element in the array.
 // if the current index is = to the last index in the array; ? is used as 'then' to then instruct 'setCurrent' (as a function) to set the state of 'current' to 0.
-// else if index is not at the last index of the array, increment the index of 'current' to the next element in the array. 
+// else if index is not at the last index of the array, increment the index state of 'current' to the next element in the array. 
         setCurrent(current === length -1 ? 0 : current + 1);
     };
 
