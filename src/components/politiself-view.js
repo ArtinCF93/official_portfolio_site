871fb9a246e3d15f8e5f2a209eac386305b3d396
ArtinCF93 <Toojool@gmail.com>
2022-06-19 23:51:45 -0700

    user personas

--- src/components/politiself-view.js
+++ src/components/politiself-view.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import imgPS from '../files/portfolio_images/AzimiArtin_CaseStudy.jpg'
-
+import imgPS1 from '../files/portfolio_images/AzimiArtin_CaseStudy_1.png';
+import imgPS2 from '../files/portfolio_images/AzimiArtin_CaseStudy_2.png';
+import imgPS3 from '../files/portfolio_images/User_Personas1.png';
+import imgPS4 from '../files/portfolio_images/User_Personas_2.png';
+import imgPS5 from '../files/portfolio_images/User_Personas_3.png';
 import './gdwork-view.css'
 
 let PolitiselfView = () => {
@@ -11,7 +14,11 @@ let PolitiselfView = () => {
                 <hr />
                 <br /><br />
                 <div className='image-container'>
-                <img src={imgPS} alt="PSscreen1" className='imgRS'/>
+                <img src={imgPS1} alt="PSscreen1" className='imgRS'/>
+                <img src={imgPS3} alt="PSscreen1" className='imgRS'/>
+                <img src={imgPS4} alt="PSscreen1" className='imgRS'/>
+                <img src={imgPS5} alt="PSscreen1" className='imgRS'/>
+                <img src={imgPS2} alt="PSscreen1" className='imgRS'/>
         </div>
     </div>
     )
